c5cc869fa3272fc1b6eb22ab2887a1be5ee4ed3b
Ray Viljoen <ray@fubra.com>
2017-09-05 12:29:26 +0200

    Removes some system settings

--- bashdots.sh
+++ bashdots.sh
@@ -14,18 +14,6 @@ BD_DIR="$(dirname "$0")"
 # Change to .bashdots
 cd "$BD_DIR"
 
-# Configure OSX Defaults
-echo 'Setting new osx defaults.'
-
-# Disable key hold
-defaults write -g ApplePressAndHoldEnabled -bool false
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -float 0.00001
-# Disable Dashboard
-defaults write com.apple.dashboard mcx-disabled -boolean YES
-# Reset Dock (After killing dashboard)
-killall Dock
-
 # Install
 function linkIt() {
 
